1431feff2ffa802e1eeccf4982c841a265faff7f
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-13 23:39:12 +0600

    Dragon News React Project Repository Added

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logo from "../assets/logo.png"
+
+const Header = () => {
+    return (
+      <div className="flex flex-col justify-center items-center mt-10 mb-8">
+        <div className="logo">
+          <img className="w-[470px] h-[60px]" src={logo} alt="Logo Image" />
+        </div>
+        <h2 className="text-lg font-normal text-[#706F6F] my-2">
+          Journalism Without Fear or Favour
+        </h2>
+      </div>
+    );
+};
+
+export default Header;
