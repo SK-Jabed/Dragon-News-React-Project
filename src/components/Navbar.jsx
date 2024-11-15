6ac6177ce2f5bb613525a570d8a0a9ebef7b4bf9
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-15 20:20:29 +0600

    Login Page Design Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { NavLink } from "react-router-dom";
-import userIcon from "../assets/user.png"
+import { Link, NavLink } from "react-router-dom";
+import userIcon from "../assets/user.jpg"
 
 const Navbar = () => {
     return (
-      <div className="flex justify-between items-center mt-5">
+      <div className="flex justify-between items-center pt-5">
         <div className=""></div>
         <div className="nav-menu text-lg font-medium text-[#706F6F] flex items-center gap-4 lg:ml-44">
           <NavLink to={"/"}>Home</NavLink>
@@ -12,7 +12,7 @@ const Navbar = () => {
         </div>
         <div className="login flex items-center gap-2">
           <img className="w-[46px]" src={userIcon} alt="User Icon" />
-          <button className="px-7 py-2 bg-[#403F3F] text-white text-lg font-semibold">Login</button>
+          <Link to={"/auth/login"} className="btn rounded-none px-7 py-2 bg-[#403F3F] text-white text-lg font-semibold hover:text-black hover:bg-white">Login</Link>
         </div>
       </div>
     );
